3e31abeaddcfe6201ebf8a900d714aa4a6e2ea57
Miller1999 <millerjavierarias@gmail.com>
2023-10-13 16:36:34 -0500

    Add education to JSON and delete from html

--- JS/education.js
+++ JS/education.js
@@ -1,12 +1,17 @@
-const containerH = document.querySelector(".contenedorItems");
+const containerF = document.querySelector(".contenedorItemsF");
 
-const createNewHobbie = (logo, title, id) => {
+const createNewEducation = (logo, title, date, institution) => {
 	const card = document.createElement("div");
-	card.classList.add("itemH");
-	card.setAttribute("id", id);
+	card.classList.add("itemF");
 	const content = `
-    <img src="${logo}" alt="${title} loading="lazy">
-    <p>${title}</p>
+    <div class="front">
+		<img loading="lazy" src="${logo}" alt="${institution}" />
+	</div>
+	<div class="back">
+		<h3>${title}</h3>
+		<p>${institution}</p>
+		<p>${date}</p>
+	</div>
   `;
 	card.innerHTML = content;
 	return card;
@@ -15,9 +20,14 @@ const createNewHobbie = (logo, title, id) => {
 fetch("../info.json")
 	.then((res) => res.json())
 	.then((data) => {
-		const hobbies = data.hobbies;
-		hobbies.forEach((hobbie) => {
-			const newHobbie = createNewHobbie(hobbie.img, hobbie.title, hobbie.id);
-			containerH.appendChild(newHobbie);
+		const education = data.education;
+		education.forEach((education) => {
+			const neweducation = createNewEducation(
+				education.logo,
+				education.title,
+				education.date,
+				education.institution
+			);
+			containerF.appendChild(neweducation);
 		});
 	});
